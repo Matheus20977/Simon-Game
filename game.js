37d6525c221a726a2a4460be73739be4ab919d4d
Matheus20977 <matheus20977@gmail.com>
2022-10-16 16:49:35 -0300

    creating new pattern

---
+++ game.js
@@ -0,0 +1,9 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.round(Math.random() * 3);
+
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
