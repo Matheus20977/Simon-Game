f08d9cac53c2b00a8f71dc9ae1a1f431b49ee5b2
Matheus20977 <matheus20977@gmail.com>
2022-10-16 17:08:45 -0300

    animation and sound to show the randomly selected button

--- game.js
+++ game.js
@@ -2,8 +2,14 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence() {
+  //randomly picks one of the colours
   var randomNumber = Math.round(Math.random() * 3);
-
   var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+  gamePattern.push(randomChosenColour); //stores the sequence in order
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100); //button flash animation
+
+  //plays the randomly picked button's audio
+  var colorAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  colorAudio.play();
 }
